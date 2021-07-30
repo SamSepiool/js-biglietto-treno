949ab9b2ad1e3054d0ef6d1f9deb2e30f4896789
SamSepiool <simone.severini@outlook.it>
2021-07-30 10:12:28 +0200

    add console.log

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 // 1) NUMERO KM DA PERCORRERE + ETA'
 var km = prompt('Quanti KM devi percorrere?');
+console.log(km);
 var eta = prompt('Quanti anni hai?');
+console.log(eta);
 // 2) CALCOLARE PREZZO VIAGGIO (0.21€/KM) 
 const PrezzoPerKm = 0.21;
 var prezzopieno = km * PrezzoPerKm;
+console.log(prezzopieno);
 // 3) VERIFICA IDONEITA' SCONTO
 var sconto;
 
@@ -14,7 +17,8 @@ if (eta < 18){
 } else {
     sconto = 0;
 }
+console.log(sconto);
 var prezzoscontato = prezzopieno - sconto;
-
+console.log(prezzoscontato);
 // 4) OUTPUT PREZZO FINALE (MAX 2 DECIMALI)
 alert('Prezzo biglietto: ' + prezzoscontato.toFixed(2) + '€'); 
